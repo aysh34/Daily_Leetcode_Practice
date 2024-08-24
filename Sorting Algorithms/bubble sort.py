d2431fdf8c5808727be151ae740521baedcad603
aysh34 <ayeshasaleem853@gmail.com>
2024-08-24 12:54:02 +0500

    update

--- Sorting Algorithms/bubble sort.py
+++ Sorting Algorithms/bubble sort.py
@@ -1,8 +1,8 @@
 def bubbleSort(arr):
     swap = False
-    for i in range(len(arr)):
-        # each step, the largest element will be at the end
-        for j in range(0,len(arr)-i-1):
+    for i in range(len(arr)):       
+        # its shows a pass
+        for j in range(0,len(arr)-i-1): # -i-1 because after each pass the largest element will be at the end
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1] = arr[j+1],arr[j]
                 swap = True
