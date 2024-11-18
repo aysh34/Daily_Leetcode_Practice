16fc220482a8cdf69814250c4f82decc30a9bf2c
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2024-11-18 10:59:05 +0500

    Update 1. Two Sum.py

--- Arrays/1. Two Sum.py
+++ Arrays/1. Two Sum.py
@@ -1,9 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # output = []
-        n = len(nums)
-        for i in range(0, n):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                        return [i,j]
-        return []
+        # n = len(nums)
+        # for i in range(0, n):
+        #     for j in range(i+1, n):
+        #         if nums[i] + nums[j] == target:
+        #                 return [i,j]
+        # return [] # TC: O(n^2)
+
+        map = {}  # TC: O(n)
+        for i in range(0, len(nums)):
+            remaining = target - nums[i]
+            if remaining in map:
+                return [map[remaining], i]
+            else:
+                map[nums[i]] = i
