322813de5287ddb231b7cc9fbaf1ed9f8e65f580
Ayesha Saleem <ayeshasaleem853@gmail.com>
2024-11-18 16:57:39 +0500

    Update 1. Two Sum.py

--- Arrays/1. Two Sum.py
+++ Arrays/1. Two Sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         # n = len(nums)
         # for i in range(0, n):
         #     for j in range(i+1, n):
