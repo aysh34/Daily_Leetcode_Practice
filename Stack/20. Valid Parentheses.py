fefa0d4fe92d5ce027a7703668f298f3905db180
Ayesha Saleem <ayeshasaleem853@gmail.com>
2024-11-21 21:21:14 +0500

    Update 20. Valid Parentheses.py

--- Stack/20. Valid Parentheses.py
+++ Stack/20. Valid Parentheses.py
@@ -15,3 +15,22 @@ class Solution:
                 stack.pop()
 
         return not stack  # return true if stack is empty else false
+
+
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         match = {")": "(", "}": "{", "]": "["}
+#         for i in s:
+#             if i in "({[":
+#                 stack.append(i)  # push in stack
+#             elif i in ")}]":
+#                 # either stack is empty or top/opening brac of the stack != respective opening brac
+#                 if (
+#                     not stack or stack[-1] != match[i]
+#                 ):  # match[i] == opening brac and i == closing brac
+#                     return False
+#                 else:
+#                     stack.pop()
+
+#         return len(stack) == 0
